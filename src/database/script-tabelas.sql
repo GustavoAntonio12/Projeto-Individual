0540f83c793235dd83e4769eab1080c9ab1ec53f
GustavoAntonio12 <gustavo.antonio@sptech.school>
2022-05-26 04:08:47 -0300

    Criacao Telas

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,10 +1,11 @@
 CREATE DATABASE projetoIndividual;
+
 USE projetoIndividual;
 
 CREATE TABLE usuario (
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 	nomeUsuario VARCHAR(45),
-	emailUsuario VARCHAR(45),
+	emailUsuario VARCHAR(45) UNIQUE,
 	telefoneUsuario CHAR(15),
 	senhaUsuario CHAR(128)
 );
